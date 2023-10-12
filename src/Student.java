fdc968cc3381e39b23062332a4b2223888b5e967
BT_1N3_28 <sample@bths.edu>
2023-10-12 11:07:30 -0400

    very committed but why?

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,24 @@
 public class Student {
+
+    private String name;
+    private double gpa;
+
+    public Student(String n, double g) {
+        name = n;
+        gpa = g;
+    }
+
+    public void helloThere() {
+        System.out.println("Hello! My name is " + name);
+    }
+
+    public void myGPA() {
+        System.out.print("My GPA is " + gpa);
+        if (gpa < 65) {
+            System.out.println("That's not good");
+        } else {
+            System.out.println("Im currently passing");
+        }
+    }
+
 }
